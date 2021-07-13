a03573a5525f1a861810e43ee9c07f077627d032
Goksubay <goksu1.9.9.8@gmail.com>
2021-07-13 09:10:59 +0300

    Project created

---
+++ Appcent_InternshipProject/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appcent_InternshipProject
+//
+//  Created by Goksubay on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
